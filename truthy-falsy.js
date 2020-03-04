721ad7facc5202e528ce54505ede92333af40e64
Nazmussakib10 <sakib.cse133@gmail.com>
2020-03-04 12:37:21 +0600

    true vs false values

---
+++ truthy-falsy.js
@@ -0,0 +1,21 @@
+//falsy values
+//const name="";
+//0
+//""
+//undefined
+//null
+//NaN
+
+//truthy value
+//"0"," ",{}.[]
+
+let name=[];
+
+
+if (name) {
+    console.log("condition is true");
+    
+} else {
+    console.log("condition is false");
+    
+}
