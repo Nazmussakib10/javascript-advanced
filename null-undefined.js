48ed86e0173cc47a91a639cbe1498a4e1b1b4e73
Nazmussakib10 <sakib.cse133@gmail.com>
2020-03-04 14:20:19 +0600

    null-undefined clear

---
+++ null-undefined.js
@@ -0,0 +1,16 @@
+let pakhi;
+function add(num1,num2=20){
+    console.log(num1+num2);
+   
+
+}
+
+const premik={name:"rahim",phone:45767}
+console.log(premik.gf);
+const result=add(10);
+console.log(result);
+let fun=undefined;
+console.log(fun);
+let ages=[23,34,45,56];
+console.log(ages[8]);
+null means empty 
