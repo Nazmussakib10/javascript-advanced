8cbaae5f9b630fdacb3535519aa21f4a7901b4b2
Nazmussakib10 <sakib.cse133@gmail.com>
2020-03-04 16:13:22 +0600

    complete if, if-else and switch statement concept

---
+++ conditional-statement.js
@@ -0,0 +1,59 @@
+
+let time=22.00 ;
+if (time>12.00) {
+    console.log("i will eat the lunch");
+    
+} else {
+    console.log("i will shower");
+}
+
+const age=20;
+if(5>9){
+    console.log("you are not right person");
+}
+if (age>=20) {
+    console.log("you are right person");
+    
+}
+if (age<20){
+    console.log("you are too young");
+    
+}
+
+var whatDate="sunday";
+if (whatDate==='friday') {
+    console.log('this is close today');
+      
+}
+if (whatDate==="saturday") {
+    console.log("thsi is saturday today");
+    
+}
+if (whatDate==='monday') {
+    console.log("this is monday");
+    
+}
+else{
+   console.log("thsi is normal working day");
+}
+
+//switch statement
+var age=25;
+switch(age){
+    case 10:
+        console.log("today is saturday,this is day");
+        break;
+    case 20:
+    console.log("this is sunday,normal working day");
+    break;
+    case 23:
+    console.log("this is monday,its also workingday");
+    break;
+    case 25:
+    console.log("this is my right age");
+    break;
+    default:
+        console.log("not a day");
+      
+
+    }
